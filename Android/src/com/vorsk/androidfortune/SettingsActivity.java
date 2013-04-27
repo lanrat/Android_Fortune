cf98f8646b36b4183e6f29ed3c410443c976ff16
Ian Foster <mrlanrat@gmail.com>
2013-04-27 02:57:59 -0700

    tried to mke the menus more versitile and formatted code

--- Android/src/com/vorsk/androidfortune/SettingsActivity.java
+++ Android/src/com/vorsk/androidfortune/SettingsActivity.java
@@ -4,27 +4,33 @@ import java.util.List;
 
 import com.actionbarsherlock.app.SherlockPreferenceActivity;
 
+import android.annotation.TargetApi;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.PreferenceFragment;
-
 
 public class SettingsActivity extends SherlockPreferenceActivity {
 
 	public static final String KEY_INTERVAL = "pref_notification_interval";
 
-
+	@SuppressWarnings("deprecation")
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 	@Override
-	protected void onPostCreate(Bundle savedInstanceState) {
-		super.onPostCreate(savedInstanceState);
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-		//Display fragment as main content
-		getFragmentManager().beginTransaction().replace(android.R.id.content,
-				new Prefs1Fragment()).commit();
-		
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		// Display fragment as main content
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
+			addPreferencesFromResource(R.xml.preferences);
+		} else {
+			getFragmentManager().beginTransaction()
+					.replace(android.R.id.content, new Prefs1Fragment())
+					.commit();
+		}
 	}
 
 	/**
@@ -32,15 +38,17 @@ public class SettingsActivity extends SherlockPreferenceActivity {
 	 */
 	@Override
 	public void onBuildHeaders(List<Header> target) {
-		//Not used right now, but may switch back to headers if we have a lot of settings
-		//loadHeadersFromResource(R.xml.preference_headers, target);
+		// Not used right now, but may switch back to headers if we have a lot
+		// of settings
+		// loadHeadersFromResource(R.xml.preference_headers, target);
 	}
 
 	/**
 	 * This fragment shows the preferences for the first header.
 	 */
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 	public static class Prefs1Fragment extends PreferenceFragment implements
-	OnSharedPreferenceChangeListener {
+			OnSharedPreferenceChangeListener {
 		private ListPreference mIntervalPreference;
 
 		@Override
@@ -49,37 +57,36 @@ public class SettingsActivity extends SherlockPreferenceActivity {
 
 			// Load the preferences from an XML resource
 			addPreferencesFromResource(R.xml.preferences);
-			mIntervalPreference = (ListPreference) getPreferenceScreen().findPreference(KEY_INTERVAL);
+			mIntervalPreference = (ListPreference) getPreferenceScreen()
+					.findPreference(KEY_INTERVAL);
 		}
 
 		@Override
 		public void onResume() {
 			super.onResume();
 			// Setup the initial values
-			mIntervalPreference.setSummary(
-					getPreferenceScreen().getSharedPreferences().getString(KEY_INTERVAL, ""));
+			mIntervalPreference.setSummary(getPreferenceScreen()
+					.getSharedPreferences().getString(KEY_INTERVAL, ""));
 			// Set up a listener whenever a key changes
-			getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+			getPreferenceScreen().getSharedPreferences()
+					.registerOnSharedPreferenceChangeListener(this);
 		}
 
 		@Override
 		public void onPause() {
 			super.onPause();
 			// Unregister the listener whenever a key changes
-			getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+			getPreferenceScreen().getSharedPreferences()
+					.unregisterOnSharedPreferenceChangeListener(this);
 		}
 
-		public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
-				String key) {
+		public void onSharedPreferenceChanged(
+				SharedPreferences sharedPreferences, String key) {
 			if (key.equals(KEY_INTERVAL)) {
-				mIntervalPreference.setSummary(sharedPreferences.getString(KEY_INTERVAL, ""));
+				mIntervalPreference.setSummary(sharedPreferences.getString(
+						KEY_INTERVAL, ""));
 			}
 		}
 	}
-
-
-
-
-
 
 }
