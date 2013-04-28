aeafea87e0dbcf7e5bb690f10b3ae8b874da9d6f
Helen <penghelen@gmail.com>
2013-04-27 19:15:28 -0700

    setting it so the user's choice is displayed in api lvl 7

--- Android/src/com/vorsk/androidfortune/SettingsActivity.java
+++ Android/src/com/vorsk/androidfortune/SettingsActivity.java
@@ -12,11 +12,15 @@ import android.os.Build;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
-public class SettingsActivity extends SherlockPreferenceActivity {
+public class SettingsActivity extends SherlockPreferenceActivity implements
+	OnSharedPreferenceChangeListener  {
 
 	public static final String KEY_INTERVAL = "pref_notification_interval";
-
+	private ListPreference mIntervalPreference;
+	private SharedPreferences prefs;
+	
 	@SuppressWarnings("deprecation")
 	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 	@Override
@@ -27,11 +31,15 @@ public class SettingsActivity extends SherlockPreferenceActivity {
 		// Display fragment as main content
 		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
 			addPreferencesFromResource(R.xml.preferences);
+			 mIntervalPreference = (ListPreference) (ListPreference)getPreferenceScreen()
+		                .findPreference(KEY_INTERVAL);
 		} else {
 			getFragmentManager().beginTransaction()
 					.replace(android.R.id.content, new Prefs1Fragment())
 					.commit();
 		}
+			
+		prefs = PreferenceManager.getDefaultSharedPreferences(this);
 	}
 	
 	@Override
@@ -59,9 +67,8 @@ public class SettingsActivity extends SherlockPreferenceActivity {
 	 * This fragment shows the preferences for the first header.
 	 */
 	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-	public static class Prefs1Fragment extends PreferenceFragment implements
-			OnSharedPreferenceChangeListener {
-		private ListPreference mIntervalPreference;
+	public static class Prefs1Fragment extends PreferenceFragment{
+		public static ListPreference mIntervalPreference;
 
 		@Override
 		public void onCreate(Bundle savedInstanceState) {
@@ -72,33 +79,37 @@ public class SettingsActivity extends SherlockPreferenceActivity {
 			mIntervalPreference = (ListPreference) getPreferenceScreen()
 					.findPreference(KEY_INTERVAL);
 		}
-
-		@Override
-		public void onResume() {
-			super.onResume();
-			// Setup the initial values
-			mIntervalPreference.setSummary(getPreferenceScreen()
-					.getSharedPreferences().getString(KEY_INTERVAL, ""));
-			// Set up a listener whenever a key changes
-			getPreferenceScreen().getSharedPreferences()
-					.registerOnSharedPreferenceChangeListener(this);
-		}
-
-		@Override
-		public void onPause() {
-			super.onPause();
-			// Unregister the listener whenever a key changes
-			getPreferenceScreen().getSharedPreferences()
-					.unregisterOnSharedPreferenceChangeListener(this);
-		}
-
-		public void onSharedPreferenceChanged(
-				SharedPreferences sharedPreferences, String key) {
-			if (key.equals(KEY_INTERVAL)) {
-				mIntervalPreference.setSummary(sharedPreferences.getString(
-						KEY_INTERVAL, ""));
-			}
-		}
 	}
 
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		
+		/* slightly hacky way of getting the fragment's list preference.
+		  I tried to do it in onCreate, but the preference is still null at that point.
+		  this way, at least the deprecated code is kept together.*/
+		if ( mIntervalPreference == null )
+			mIntervalPreference = Prefs1Fragment.mIntervalPreference;
+		
+		// Setup the initial values
+		mIntervalPreference.setSummary(prefs.getString(KEY_INTERVAL, ""));
+		// Set up a listener whenever a key changes
+		prefs.registerOnSharedPreferenceChangeListener(this);
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		// Unregister the listener whenever a key changes
+		prefs.unregisterOnSharedPreferenceChangeListener(this);
+	}
+
+	public void onSharedPreferenceChanged(
+			SharedPreferences sharedPreferences, String key) {
+		if (key.equals(KEY_INTERVAL)) {
+			mIntervalPreference.setSummary(sharedPreferences.getString(
+					KEY_INTERVAL, ""));
+		}
+	}
 }
