696bd8187847a45edbf0816d9aefa56ff3c9aa02
Ian Foster <mrlanrat@gmail.com>
2013-06-02 15:33:55 -0700

    widget buttons work again

--- Android/src/com/vorsk/androidfortune/FortuneWidgetIntentReceiver.java
+++ Android/src/com/vorsk/androidfortune/FortuneWidgetIntentReceiver.java
@@ -3,9 +3,12 @@ package com.vorsk.androidfortune;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 public class FortuneWidgetIntentReceiver extends BroadcastReceiver{
+	
+	private static final String TAG = "WidgetIntentReciever";
 	
 	/** Receives the intent and checks if it is up vote or 
 	 * down vote then calls the appropriate listener. Also
@@ -15,41 +18,51 @@ public class FortuneWidgetIntentReceiver extends BroadcastReceiver{
 	 */
 	@Override
 	public void onReceive(final Context context, Intent intent) {
-		
+		Log.v(TAG,"onRecieve");
 		final RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
 		final Fortune current_fortune = Client.getInstance(context).getCurrentFortune();
 		String vote_message = null;
 		
 		if (current_fortune != null)
 		{
-		
+			Log.v(TAG,"fortune not null");
 			if(intent.getAction().equals("up_button.intent.action.UP_VOTE")){
 				vote_message = "Up Vote Clicked :-D";
 				WidgetActivity.countUpVote();
+				remoteViews.setOnClickPendingIntent(R.id.up_button, FortuneWidgetProvider.buildUpButtonPendingIntent(context));
 			}
 			else if(intent.getAction().equals("down_button.intent.action.DOWN_VOTE")){
 				vote_message = "Down Vote Clicked :-(";
+				remoteViews.setOnClickPendingIntent(R.id.down_button, FortuneWidgetProvider.buildDownButtonPendingIntent(context));
 				WidgetActivity.countDownVote();
 			}
 			
 			if (vote_message != null)
 			{
+				Log.v(TAG,"message not null");
 				remoteViews.setTextViewText(R.id.fortune_view, vote_message);
 				// creating a timer thread to set text view back to the fortune String
 				Thread timer = new Thread(){
 					public void run(){
+						Log.v(TAG,"sarting thread");
 						try{
 							sleep(600);
 						} catch(InterruptedException e){
-							e.printStackTrace();
+							Log.e(TAG,"thread exception");
 						} finally{
 							remoteViews.setTextViewText(R.id.fortune_view, current_fortune.getFortuneText(true));
+							FortuneWidgetProvider.pushWidgetUpdate(context.getApplicationContext(), remoteViews);
 						}
 					}
 				};
 				timer.start();
+			}else{
+				Log.e(TAG, "vote message is null!");
 			}
 		}
+		
+		FortuneWidgetProvider.pushWidgetUpdate(context.getApplicationContext(), remoteViews);
+		
 	}// end method
 	
 	/** Counts up vote as user presses up button and notify's
